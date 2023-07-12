0b3872ae60d112b37a1a445791f3a81736a4e2de
youssefaloua <alouani.y.fst@uhp.ac.ma>
2023-07-12 18:35:29 +0000

    ADD CORS:POLICYY

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const {ENV_MODE  } = require('./config')
 const { MediaRouter } = require('./modules/index')
 var app = express();
 app.use(logger(ENV_MODE));
-
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);app.use('/media/v1.0.0/',MediaRouter)
-
+);
+app.use(function corsAllow(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+})
+app.use('/media/v1.0.0/',MediaRouter)
 app.use(function(err, req, res, next) {
   console.error('err',err)
   res.status(400);
