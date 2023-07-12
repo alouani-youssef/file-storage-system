bad1932d22e56018d5e523947720d8b45c32f183
youssefaloua <alouani.y.fst@uhp.ac.ma>
2023-07-12 19:00:01 +0000

    ADD SUPPORT ON NEW MEDIA FORMATS

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 exports.PORT = 60014;
 exports.ENV_MODE = 'dev'
-exports.DEFAULT_URL = 'http://localhost:60014/media/v1.0.0/file/';
+exports.DEFAULT_URL = 'http://146.190.121.194:60014/media/v1.0.0/file/';
 exports.DEFAULT_TYPE = 'MEDIA:IMAGE';
 exports.MAX_FILE_SIZE = (32 * 1024 * 1024) // 32MB;
 exports.SUPPORTED_FORMATS = ['JPEG','JPNG','PDF','XLSX','CSV','DOC','MP3','MP4','GIF','HEIC','JPG']
